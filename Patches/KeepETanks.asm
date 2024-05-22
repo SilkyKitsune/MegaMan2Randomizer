30907f5ffdd8a3609968a75cc7543b8a2b289fd8
Jordan Little <jerd.12345@gmail.com>
2024-05-22 16:05:51 -0500

    smol notes

--- Patches/KeepETanks.asm
+++ Patches/KeepETanks.asm
@@ -1,10 +1,12 @@
 ﻿; Just fyi idk if this will work in an assembler, it was hand assembled/disassembled
 
-LDA #$0E  ; A9 0E
-JSR $C000 ; 20 00 C0
-DEC $A8   ; C6 A8
-BNE $1B   ; D0 1B
-LDA #$00  ; A9 00    ; NOP NOP   ; EA EA    ; 0x03_C1_CA
-STA $A7   ; 85 A7    ; NOP NOP   ; EA EA
-LDA #$0D  ; A9 0D
-JSR $C000 ; 20 00 C0
+Instructions ; Bytes    ; File Addr  ; Replacement ; Bytes    ; Comments
+-------------------------------------------------------------------------
+LDA #$0E     ; A9 0E    ;            ;             ;          ;
+JSR $C000    ; 20 00 C0 ;            ;             ;          ;
+DEC $A8      ; C6 A8    ;            ;             ;          ;
+BNE $1B      ; D0 1B    ;            ;             ;          ;
+LDA #$00     ; A9 00    ; 0x03_C1_CA ; NOP NOP     ; EA EA    ; 
+STA $A7      ; 85 A7    ;            ; NOP NOP     ; EA EA    ;
+LDA #$0D     ; A9 0D    ;            ;             ;          ;
+JSR $C000    ; 20 00 C0 ;            ;             ;          ;
