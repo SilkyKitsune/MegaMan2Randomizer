bb8585d3a8de5a14f206ac4cbef73500973a3283
SilkyKitsune <wavemusictv@gmail.com>
2026-05-22 00:32:05 -0500

    Added SuperNintendo to MM1 patch validation

--- PatchManager.cs
+++ PatchManager.cs
@@ -160,7 +160,7 @@ public static class PatchManager
         }
 
         errors +=
-            ValidatePatches(GameID.MM1, PatchID.MagnetBeamBuff, PatchID.AtomicFireFix, VersionID.Japan, VersionID.NorthAmerica) +
+            ValidatePatches(GameID.MM1, PatchID.MagnetBeamBuff, PatchID.AtomicFireFix, VersionID.Japan, VersionID.NorthAmerica, VersionID.SuperNintendo) +
             ValidatePatches(GameID.MM2, PatchID.AtomicFireFix, PatchID.Count, VersionID.Japan, VersionID.NorthAmerica, VersionID.SuperNintendo);
 
         return errors.Length > 0;
